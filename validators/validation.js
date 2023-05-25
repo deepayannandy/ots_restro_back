b6d5f03a54eab5c5ab50b5020d60de9b75aef38f
Deepayan Nandy <deepayan.622@gmail.com>
2023-05-25 13:43:07 +0530

    25.5.23_2

--- validators/validation.js
+++ validators/validation.js
@@ -11,6 +11,7 @@ const schema=joi.object().keys({
     UserStatus:joi.bool().required(),
     restroname:joi.string().required(),
     nickname:joi.string().required(),
+    imageURL:joi.string().required(),
 });
 return schema.validate(data);
 }
